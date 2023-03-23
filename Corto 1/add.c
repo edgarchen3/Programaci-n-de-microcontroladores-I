9c6d8fff0b4e2a0f1690dd3ea74261774d8e4b8e
edgarchen3 <84102693+edgarchen3@users.noreply.github.com>
2023-03-23 09:30:34 -0600

    Add files via upload

---
+++ Corto 1/add.c
@@ -0,0 +1,18 @@
+#include "add.h"
+
+int add (int a, int b) {
+    return a + b;
+}
+
+int dif (int a, int b) {
+    return a - b;
+}
+
+int mul (int a, int b) {
+    return a*b;
+}
+
+int div (int a, int b) {
+    return a / b;
+}
+
---
+++ Corto 1/add.h
@@ -0,0 +1,10 @@
+#ifndef ADD_H
+#define ADD_H
+#define PI 3.14159
+
+int add (int a, int b);
+int dif (int a, int b);
+int mul (int a, int b);
+int div (int a, int b);
+
+#endif  /*ADD_H*/
